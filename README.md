953092c16bc33de06a91e9de7b2c18b690ff8915
Michael Kraft <michael-kraft@outlook.com>
2017-03-08 18:12:06 -0600

    Updated spotify, fixed Chrome

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Ubuntu-Configure-Script
 Scripts to configure a fresh install of Ubuntu 14.04.
 
-## configure-ubuntu-box.sh
+### Settings:
+* Adds myself to sudoers.d
 
 ### Installs:
 
@@ -16,7 +17,4 @@ Scripts to configure a fresh install of Ubuntu 14.04.
 
 *More will likely be added down the line.*
 
-### Other
 
-* Adds a link to my devbox
-* Adds me to sudoers
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,13 @@
-# Add me to sudoers
-sudo echo "michael ALL = NOPASSWD : ALL" > /etc/sudoers.d/michael
-
 ##### Add keys
 cd /tmp
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 wget http://download.opensuse.org/repositories/isv:ownCloud:desktop/Ubuntu_14.04/Release.key
 sudo apt-key add - < Release.key
+sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 cd -
 
 # Add Repositories
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo add-apt-repository ppa:webupd8team/sublime-text-2 -y
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud-client.list"
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
@@ -19,21 +16,27 @@ echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sourc
 sudo apt-get update
 
 # Install
-sudo apt-get install google-chrome-stable
+sudo apt-get install spotify-client -y
+sudo apt-get install google-chrome-stable -y
 sudo apt-get install sublime-text -y
+sudo apt-get install kubuntu-desktop -y
 sudo apt-get install owncloud-client -y
 sudo apt-get install git -y
 sudo apt-get install vim -y
 sudo apt-get install screen -y
-sudo apt-get install cifs-tools -y
-sudo apt-get install spotify-client -y
-sudo apt-get install kubuntu-desktop -y
+sudo apt-get install cifs-utils -y
 
-# Update lists, then update everything for good measure.
+# Chrome's repo breaks apt later and it updates by itself anyway, this isn't needed.
+sudo rm -f /etc/apt/sources.list.d/google*
+
+# Add myself to sudoers.d
+echo "michael ALL = NOPASSWD : ALL" > michael.temp
+sudo chown root michael.temp
+sudo chgrp root michael.temp
+sudo mv michael.temp /etc/sudoers.d/michael
+
+# Update lists, then update everything for good measure and reboot.
 sudo apt-get update
 sudo apt-get upgrade -y
 
-# Add devbox to /etc/fstab
-sudo echo "//genesis/devbox /media/devbox cifs username=michael,password=######,iocharset=utf8,sec=ntlm 0 0" >> /etc/fstab
-
-
+sudo shutdown -r now
